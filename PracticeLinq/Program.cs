7a9a1916d224cd1d592590febb60da6b55360389
jmmascorro <jessemascorro88@gmail.com>
2022-02-25 06:39:25 -0600

    FinishedPracticeLinqExercise

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGamesList = new List<string>()
+            {
+                "Super Mario Bros",
+                "Dark Souls",
+                "Crash Bandicoot",
+                "Red Dead Redemption 2",
+                "Metal Gear Solid",
+                "Zork"
+            };
+
+            var orderedGames = videoGamesList.OrderBy(game => game.Length).ToList();
+
+            //foreach(var game in orderedGames)
+            //{
+            //    Console.WriteLine(game);
+            //}
+
+            orderedGames.ForEach(game => Console.WriteLine(game));
+        }
+    }
+}
